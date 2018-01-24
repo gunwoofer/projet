8b42b0fd1c179a7850364b8816a090bffab16717
Valentin Bouis <valentin.bouis@gmail.com>
2018-01-24 14:39:08 -0500

    creation d un objet etudiant lorsqu on rejoint une session

--- client/src/app/detailsSession/detailsSession.component.ts
+++ client/src/app/detailsSession/detailsSession.component.ts
@@ -1,3 +1,4 @@
+import { Etudiant, Genie } from './../etudiant/etudiant';
 import { AuthService } from './../authentification/authService.service';
 import { ListeSessionService } from './../listeSession/listeSessionService.service';
 import { RouterModule } from '@angular/router';
@@ -16,7 +17,12 @@ export class DetailsSessionComponent implements OnInit {
 
     public session: Session;
     public profil: any;
+
     public prenom: string;
+    public nom: string;
+    public genie: string;
+    public mail: string;
+    public avatar: string;
 
     constructor(private router: Router, private listeSessionService: ListeSessionService, private auth: AuthService) {
 
@@ -32,7 +38,12 @@ export class DetailsSessionComponent implements OnInit {
             this.auth.getProfile((err, profile) => {
               self.profil = profile;
               console.log(self.profil);
+
               this.prenom = self.profil['http://revise-pas-seul/prenom'];
+              this.nom = self.profil['http://revise-pas-seul/nom'];
+              this.genie = self.profil['http://revise-pas-seul/genie'];
+              this.mail = self.profil.name;
+              this.avatar = self.profil.picture;
             });
           }
 
@@ -40,6 +51,29 @@ export class DetailsSessionComponent implements OnInit {
 
     public estConnecte(): boolean {
         return this.auth.isAuthenticated();
+    }
+
+    public rejoindreSession(): void {
+        let genie: Genie;
+
+        switch (this.genie) {
+            case('informatique'): { genie = Genie.Informatique ; break; }
+            case('logiciel'): { genie = Genie.Logiciel ; break; }
+            case('civil'): { genie = Genie.Civil ; break; }
+            case('chimique'): { genie = Genie.Chimique ; break; }
+            case('industriel'): { genie = Genie.Industriel ; break; }
+            case('physique'): { genie = Genie.Physique ; break; }
+            case('mecanique'): { genie = Genie.Mecanique ; break; }
+            case('aero'): { genie = Genie.AeroSpatial ; break; }
+            case('bio'): { genie = Genie.BioMedical ; break; }
+            case('electrique'): { genie = Genie.Electrique ; break; }
+            case('geo'): { genie = Genie.Geologique ; break; }
+            case('mine'): { genie = Genie.Mines ; break; }
+            default : { genie = null ; }
+        }
+
+        const etudiant = new Etudiant(this.prenom, this.nom, genie, this.mail, this.avatar);
+        console.log(etudiant);
     }
 
 
--- client/src/app/etudiant/etudiant.ts
+++ client/src/app/etudiant/etudiant.ts
@@ -1,7 +1,37 @@
+
+export enum Genie {
+    Informatique = 'Informatique',
+    Logiciel = 'Logiciel',
+    Civil = 'Civil',
+    Chimique = 'Chimique',
+    Industriel = 'Industriel',
+    Physique = 'Physique',
+    Mecanique = 'Mecanique',
+    AeroSpatial = 'Aéro-Spatial',
+    BioMedical = 'Bio-médical',
+    Electrique = 'Electrique',
+    Geologique = 'Géologique',
+    Mines = 'Mines'
+}
+
 export class Etudiant {
     private prenom: string;
     private nom: string;
     private matricule: string;
+    private genie: Genie;
+    private mail: string;
+    private avatar: string; // Lien gravatar
 
-    constructor() {}
+
+    constructor(prenom: string, nom: string, genie: Genie, mail: string, avatar: string) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.genie = genie;
+        this.mail = mail;
+        this.avatar = avatar;
+    }
+
+    public setMatricule(matricule: string): void {
+        this.matricule = matricule;
+    }
 }
